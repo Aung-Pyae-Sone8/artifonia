2856f6c504ac263204c4bf5749264dd2a416f0e3
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2025-01-07 01:30:25 +0630

    preparing-onboarding-animated-slide

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SegoeUI: require('../assets/fonts/Segoe-UI.ttf'),
   });
 
   useEffect(() => {
@@ -37,7 +38,8 @@ export default function RootLayout() {
         <Stack.Screen name="index"/>
         <Stack.Screen name="(tabs)"/>
         <Stack.Screen name='/(routes)/onboarding'/>
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='/(routes)/home'/>
+        {/* <Stack.Screen name="+not-found" /> */}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
